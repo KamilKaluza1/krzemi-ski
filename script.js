3c9a161b8c74f2c4208ee59ddd290b5907e1f7e3
Kamil Kałuża <kamil321.94@gmail.com>
2022-12-16 07:43:57 +0100

    init

---
+++ script.js
@@ -0,0 +1,15 @@
+const observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+      const square = entry.target.querySelector('.witaj-img');
+  
+      if (entry.isIntersecting) {
+        square.classList.add('witaj-animation');
+        return; // if we added the class, exit the function
+      }
+  
+      // We're not intersecting, so remove the class!
+      square.classList.remove('witaj-animation');
+    });
+  });
+  
+  observer.observe(document.querySelector('.square-wrapper'));
